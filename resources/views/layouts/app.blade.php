f930d182df9f53bc806c72d46f217b848fcc1225
Admin <admin@example.com>
2026-05-27 15:12:11 +0700

    Tambah PSB dan ITJ

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
                         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"></path>
                         </svg>
-                        <span class="hidden sm:inline">SP3DA</span>
+                        <span>SP3DA</span>
                     </a>
                     
                     <!-- Desktop Menu -->
